9f17027abe1b9937602979ab1683c4bac1ecfeed
dorameme <doradorav@naver.com>
2023-09-11 13:19:56 +0900

    유저 조회 API 개발과 테스트

--- src/main/java/com/group/libraryapp/domain/user/User.java
+++ src/main/java/com/group/libraryapp/domain/user/User.java
@@ -7,9 +7,17 @@ public class User {
     private Integer age;
     public User(String name,Integer age){
         if(name == null || name.isBlank()){
-            throw new IllegalArgumentException(String.format("잘못된 name(%s)이 들어왔습니다",name);
+            throw new IllegalArgumentException(String.format("잘못된 name(%s)이 들어왔습니다",name));
         }
         this.name=name;
         this.age=age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
     }
 }
